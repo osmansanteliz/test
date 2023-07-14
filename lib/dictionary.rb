738e9c4142b05a0f64c9f2398183670c2477b891
Osman Santeliz <osanteliz88@gmail.com>
2023-07-14 13:25:56 -0600

    * Archivos editados para poder pasar el test.

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,1 +1,12 @@
 # Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/dictionary_test.rb pasen
+
+class Dictionary
+  def initialize(words)
+    @words = words
+  end
+
+  def find_meaning(word)
+    found_word = @words.find {|w| w.word == word}
+    found_word&.meaning
+  end
+end
